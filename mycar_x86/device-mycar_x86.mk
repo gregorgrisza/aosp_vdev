98300c8db3b53e73ee2b7243b18b3495517ce2b8
Grzegorz Michalak <gregorgrisza@gmail.com>
2025-02-17 19:31:01 +0100

    WIP: adds emulator and move to cuttlefish device

--- mycar_x86/device-mycar_x86.mk
+++ mycar_x86/device-mycar_x86.mk
@@ -1,7 +1,8 @@
 PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
 
+# $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86.mk)
+$(call inherit-product, device/google/cuttlefish/vsoc_x86_64/phone/aosp_cf.mk)
 
 # To fix Audio HAL Error - "No device HAL manifest: No such device"
 EMULATOR_VENDOR_NO_SOUND := true
