78e129aa6aa496b2c201e49617f86b603e023dd8
Grzegorz Michalak <grzegorz.michalak@globallogic.com>
2025-02-18 23:16:30 +0100

    Add custom actions to cf

--- mycar_cf_x86/device-mycar_cf_x86.mk
+++ mycar_cf_x86/device-mycar_cf_x86.mk
@@ -2,5 +2,6 @@
 
 $(call inherit-product, device/google/cuttlefish/vsoc_x86_64_only/auto/aosp_cf.mk)
 
+
 # To fix Audio HAL Error - "No device HAL manifest: No such device"
 EMULATOR_VENDOR_NO_SOUND := true
--- mycar_cf_x86/mycar_cf_x86.mk
+++ mycar_cf_x86/mycar_cf_x86.mk
@@ -8,3 +8,14 @@ PRODUCT_MODEL := mycar cuttlefish
 PRODUCT_MANUFACTURER := v
 
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+$(call soong_config_set, cvd, custom_action_config, my_custom_action_config.json)
+$(call soong_config_append, cvd, custom_action_servers, cuttlefish_mycar_action_server)
+
+SOONG_CONFIG_NAMESPACES += cvd
+
+SOONG_CONFIG_cvd += custom_action_config
+SOONG_CONFIG_cvd_custom_action_config := mycar_action_config.json
+
+SOONG_CONFIG_cvd += custom_action_servers
+SOONG_CONFIG_cvd_custom_action_servers += cuttlefish_mycar_action_server
